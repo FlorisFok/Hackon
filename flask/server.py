9149a05a06588f383a4312e1ea9bc6533ede33e5
Oscar Mulder <oscar.mulder@live.nl>
2019-04-06 09:33:23 +0200

    Layout and server changes

--- flask/server.py
+++ flask/server.py
@@ -10,15 +10,16 @@ from PyPDF2 import PdfFileWriter, PdfFileReader
 from flask import jsonify
 import io
 import sys
+import re
 app = Flask(__name__)
 
-def convert_pdf(filename, output_path):
+def convert_pdf(filename, output_path, pagenumber):
     input_file = 'diditwork'
     print("Start")
     print("Done")
 
     inp = PdfFileReader(filename, "rb")
-    page = inp.getPage(7)
+    page = inp.getPage(pagenumber)
 
     wrt = PdfFileWriter()
     wrt.addPage(page)
@@ -43,52 +44,86 @@ def crop_image(filename, area = (400, 400, 800, 800)):
     cropped_img = png_img.crop(area)
     return cropped_img
 
-# def convert_pdf(filename, output_path, resolution=300):
-#     """ Convert a PDF into images.
+def pos_neg_calc(list):
+    display_dict = {
+        'succes': True,
+        'total': 0,
+        'totalparsed': 0,
+        'records': []
+    }
+    for e in list[:-1]:
+        if e:
+            f = e.replace(',', '').replace(' ', '').replace('.', '')
+            if ("(" in f and ")" in f) or "-" in f:
+                f = f.replace('(', '').replace(')', '').replace('-', '')
+                display_dict['total'] -= int(f)
+                display_dict['records'].append(-int(f))
+            else:
+                display_dict['total'] += int(f)
+                display_dict['records'].append(int(f))
+    last = list[-1]
+    if last:
+        l = last.replace(',', '').replace(' ', '').replace('.', '')
+        if ("(" in f and ")" in f) or "-" in f:
+            l = l.replace('(', '').replace(')', '').replace('-', '')       
+            display_dict['totalparsed'] = -int(l)
+        else:
+            display_dict['totalparsed'] = int(l)
+    return display_dict
 
-#         All the pages will give a single png file with format:
-#         {pdf_filename}-{page_number}.png
-
-#         The function removes the alpha channel from the image and
-#         replace it with a white background.
-#     """
-#     all_pages = Image(filename=filename, resolution=resolution)
-#     for i, page in enumerate(all_pages.sequence):
-#         with Image(page) as img:
-#             img.format = 'png'
-#             img.background_color = Color('white')
-#             img.alpha_channel = 'remove'
-
-            # image_filename = os.path.splitext(os.path.basename(filename))[0]
-            # image_filename = '{}-{}.png'.format(image_filename, i)
-            # image_filename = os.path.join(output_path, image_filename)
-
-#             img.save(filename=image_filename)
-#             return (image_filename)
-
-
-
-def read_image(filename):
+def read_image(filename, point=False):
     im = PImage.open(filename)
     text = pytesseract.image_to_string(im, lang = 'eng')
-    data = text.split("\n")
-    total = 0
+
+    # digits = []
+    # if len(numbers) > 0:
+    #     if point:
+    #         for num in numbers[:-1]:
+    #             try:
+    #                 total += float(num[0])+float(num[1])*0.1
+    #             except:
+    #                 total += float(num[0])
+    #     else:
+    #         for num in numbers[:-1]:
+    #             number = ''
+    #             for s in num:
+    #                 number += s
+    #             total += int(number)
+    #             digits.append(int(number))
+            # for s in numbers[-1]:
+            #     totalpic += s
+    display_dict = pos_neg_calc(text.split('\n'))
     print(text)
     print("---------- PARSED DATA ----------")
-
-    for d in data[:-2]:
-        euro = float(d.replace(',', '').replace(' ', ''))
-        total += euro
-        print(euro)
+    for d in display_dict['records']:
+        print(d)
     print("-------------- +")
-    totalpic = float(data[-1].replace(',', '').replace(' ', ''))
-    print("Parsed total:		", totalpic)
-    print("Calculated total:	", total)
+    print("Parsed total:		", display_dict['totalparsed'])
+    print("Calculated total:	", display_dict['total'])
+    print("-----------------------------------")
+    print("Posneg:")
+    print()
+    # data = text.split("\n")
+    # total = 0
+    # print(text)
+    # print("---------- PARSED DATA ----------")
 
-    if (total == totalpic):
-        print("Correct!")
-    else:
-        print("Incorrect!")
+    # for d in data[:-2]:
+    #     euro = float(d.replace(',', '').replace(' ', ''))
+    #     total += euro
+    #     print(euro)
+    # print("-------------- +")
+    # totalpic = float(data[-1].replace(',', '').replace(' ', ''))
+    # print("Parsed total:		", totalpic)
+    # print("Calculated total:	", total)
+
+    # if (total == totalpic):
+    #     print("Correct!")
+    # elif ((total - 1) == totalpic or (total + 1) == totalpic):
+	#     print("Rounding error!")
+    # else:
+    #     print("Incorrect!")
+    return display_dict
 
 @app.route('/')
 def hello():
@@ -98,10 +133,16 @@ def hello():
 def upload_file():
     if request.method == 'POST':
         f = request.files['the_file']
+        pagenumber = request.form['pagenum']
+        if not pagenumber:
+            pagenumber = 1
+        else:
+            pagenumber = int(pagenumber)
+        pagenumber -= 1
         filename = secure_filename(f.filename)
         path = os.path.join('/Users/oscar/Development/EY-hackathon/Hackon/flask/uploads', filename)
         f.save(path)
-        filen = convert_pdf(path, "./static")
+        filen = convert_pdf(path, "./static", pagenumber)
         return render_template('next.html', img=filen)
 
 @app.route("/image", methods=["POST"])
@@ -113,11 +154,11 @@ def check():
     filename = request.form.get("imgsrc")
     if x1 and y1 and x2 and y2 and filename:
         print(x1, " ", y1, " ", x2, " ", y2, " ", filename)
-        area = (int(x1), int(y1), int(x2), int(y2))
+        area = (int(x1) * 2, int(y1) * 2, int(x2) * 2, int(y2) * 2)
         image = crop_image(filename, area)
         path = os.path.join('/Users/oscar/Development/EY-hackathon/Hackon/flask/uploads', "out.png")
         image.save(path, "PNG")
-        read_image(path)
-        return jsonify({"success": True})
+        display_dict = read_image(path)
+        return jsonify(display_dict)
     else:
         return jsonify({"success": False})
